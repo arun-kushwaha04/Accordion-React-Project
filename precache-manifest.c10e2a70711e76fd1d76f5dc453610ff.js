fe49a62d9172a461b107069f149f2afcdd5692d3
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-06-30 11:36:10 +0530

    Updates

---
+++ precache-manifest.c10e2a70711e76fd1d76f5dc453610ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee13a297773941daf0c7d44139d3827",
+    "url": "/Accordion-React-Project/index.html"
+  },
+  {
+    "revision": "457da53b92be08e0ffb9",
+    "url": "/Accordion-React-Project/static/css/main.5ebca7d9.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/Accordion-React-Project/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Accordion-React-Project/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457da53b92be08e0ffb9",
+    "url": "/Accordion-React-Project/static/js/main.cc008fed.chunk.js"
+  },
+  {
+    "revision": "0e530632080b163b451c",
+    "url": "/Accordion-React-Project/static/js/runtime-main.86c8672a.js"
+  }
+]);
